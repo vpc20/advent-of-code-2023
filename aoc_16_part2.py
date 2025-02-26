66262821d85a943bbdb4eac7942381b1341fca1f
vpc20 <cuevasvp@yahoo.com>
2025-02-26 15:45:44 +0800

    aoc 2023

--- aoc_16_part2.py
+++ aoc_16_part2.py
@@ -1,6 +1,6 @@
 from collections import deque
 
-from aoc_tools import read_input_to_grid, print_grid_as_text
+from aoc_tools import read_input_to_grid
 
 # directions
 NORTH = (-1, 0)
@@ -26,13 +26,13 @@ dir_dict = {(NORTH, '/'): (EAST,),
             }
 
 
-def beam_traversal(grid):
+def beam_traversal(grid, pos, direction):
     nrows = len(grid)
     ncols = len(grid[0])
 
-    pos = (0, 0)
+    # pos = (0, 0)
     # direction = EAST # for test data
-    direction = SOUTH
+    # direction = SOUTH
 
     q = deque()
     q.append((pos, direction))
@@ -55,30 +55,43 @@ def beam_traversal(grid):
                     dr, dc = direction
                 elif grid[nr][nc] == '-' or grid[nr][nc] == '|':
                     if (direction, grid[nr][nc]) in dir_dict:
-                        direction1, direction2 = dir_dict[(direction, grid[nr][nc])]
-                        q.append(((nr, nc), direction1))
-                        q.append(((nr, nc), direction2))
+                        for direction in dir_dict[(direction, grid[nr][nc])]:
+                            q.append(((nr, nc), direction))
                         break
                 r, c = nr, nc
             else:
                 break
 
-    grid_copy = grid.copy()
-    for pos, direction in visited:
-        r, c = pos
-        grid[r][c] = '#'
+    # grid_copy = grid.copy()
+    # for pos, direction in visited:
+    #     r, c = pos
+    #     grid[r][c] = '#'
 
-    for e in grid:
-        print(e)
-    print_grid_as_text(grid)
+    # for e in grid:
+    #     print(e)
+    # print_grid_as_text(grid)
 
     print(len(visited))
     unique_pos = {pos for pos, direction in visited}
-    print(len(unique_pos))
+    print(len(unique_pos))  # unique number of positions vistited are the energized tiles
+    return len(unique_pos)
 
 
 if __name__ == '__main__':
     # grid = read_input_to_grid('aoc_16_test_data1.txt')
     grid = read_input_to_grid('aoc_16_data1.txt')
 
-    beam_traversal(grid)
+    # pos, direction = initial_pos_dir(grid)
+    nrows = len(grid)
+    ncols = len(grid[0])
+
+    max_energized = 0
+    for j in range(ncols):
+        max_energized = max(max_energized, beam_traversal(grid, (0, j), SOUTH))
+        max_energized = max(max_energized, beam_traversal(grid, (nrows - 1, j), NORTH))
+
+    for i in range(nrows):
+        max_energized = max(max_energized, beam_traversal(grid, (i, 0), EAST))
+        max_energized = max(max_energized, beam_traversal(grid, (i, ncols - 1), WEST))
+
+    print(max_energized)
