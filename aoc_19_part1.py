ca24b7791d1f4791aa858caa79b148411ab23901
vpc20 <cuevasvp@gmail.com>
2025-03-30 17:30:20 +0800

    aoc 2023

--- aoc_19_part1.py
+++ aoc_19_part1.py
@@ -2,26 +2,48 @@ from collections import defaultdict
 
 from aoc_tools import read_input_to_sections
 
+
+def convert_to_dict(text):
+    result = {}
+    wf_code, wf_dtl = line.split('{')
+    wf_dtls = wf_dtl[:-1]  # discard '}'
+
+    wf_rules = []
+    for wf_dtl in wf_dtls.split(','):
+        if ':' in wf_dtl:
+            k, v = wf_dtl.split(':')
+            wf_rules.append({k:v})
+        else:
+            wf_rules.append({wf_dtl:None})
+
+    result[wf_code] = wf_rules
+    return result
+
+
+def convert_to_list(text):
+    result = []
+    for rating in text.strip().lstrip('{').rstrip('}').split(','):
+        k, v = rating.split('=')
+        result.append({k:v})
+    return result
+
+
 if __name__ == '__main__':
     sections = read_input_to_sections('aoc_19_test_data1.txt')
     # print(len(sections))
     # print(sections[0])
     # print(sections[1])
 
-    # d1 = defaultdict()
-    # for line in sections[0].strip().split():
-    #     print(line)
-    #     l1 = line.split('{')
-    #     print(l1)
-    #     d1[l1[0]] = l1[1][:-1]
-    # print('----------------')
-    # print(d1)
+    # workflows = [line for line in sections[0].strip().split()]
+    # wf_dict = {e.split('{')[0]: e.split('{')[1][:-1] for e in workflows}
+    # print(wf_dict)
 
-    workflows = [line for line in sections[0].strip().split()]
-    wf_dict = {e.split('{')[0]: e.split('{')[1][:-1] for e in workflows}
-    print(wf_dict)
+    for line in sections[0].strip().split():
+        wf_dict = convert_to_dict(line)
+        print('wf_dict', wf_dict)
 
-    # d1 = {x[0]: x[1][:-1] for x in line.split('{')}
-    # print(d1)
+    for line in sections[1].strip().split():
+        ratings_list = convert_to_list(line)
+        print('ratings_list', ratings_list)
 
-    # px:['a<2006':'qkq', 'm > 2090': 'A', rfg]
+
