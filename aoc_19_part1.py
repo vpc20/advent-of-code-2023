df55cba2b96a1a53f7a8b84750a6c051d0ae31e5
vpc20 <cuevasvp@gmail.com>
2025-03-31 17:33:40 +0800

    aoc 2023

--- aoc_19_part1.py
+++ aoc_19_part1.py
@@ -1,31 +1,33 @@
-from collections import defaultdict
+# from collections import defaultdict
+from collections import OrderedDict
 
 from aoc_tools import read_input_to_sections
 
 
-def convert_to_dict(text):
-    result = {}
-    wf_code, wf_dtl = line.split('{')
-    wf_dtls = wf_dtl[:-1]  # discard '}'
-
-    wf_rules = []
+def convert_rules_to_dict(text):
+    result = OrderedDict()
     for wf_dtl in wf_dtls.split(','):
         if ':' in wf_dtl:
             k, v = wf_dtl.split(':')
-            wf_rules.append({k:v})
+            result[k] = v
         else:
-            wf_rules.append({wf_dtl:None})
-
-    result[wf_code] = wf_rules
+            result[wf_dtl] = None
     return result
 
 
-def convert_to_list(text):
-    result = []
+def convert_ratings_to_dict(text):
+    result = OrderedDict()
     for rating in text.strip().lstrip('{').rstrip('}').split(','):
         k, v = rating.split('=')
-        result.append({k:v})
+        result[k] = v
     return result
+
+
+def apply_workflow(ratings_dict, wf_dict):
+    for part_code, part_val in ratings_dict.items():
+        print(part_code, part_val)
+        rules_dict = wf_dict['in']
+        pass
 
 
 if __name__ == '__main__':
@@ -38,12 +40,26 @@ if __name__ == '__main__':
     # wf_dict = {e.split('{')[0]: e.split('{')[1][:-1] for e in workflows}
     # print(wf_dict)
 
+    wf_dict = {}
     for line in sections[0].strip().split():
-        wf_dict = convert_to_dict(line)
-        print('wf_dict', wf_dict)
+        wf_code, wf_dtls = line.strip().split('{')
+        wf_dtls = wf_dtls.rstrip('}')
+        rules_dict = convert_rules_to_dict(wf_dtls)
+        wf_dict[wf_code] = rules_dict
 
+    print('wf_dict')
+    for k, v in wf_dict.items():
+        print(k, v)
+
+    ratings_list = []
     for line in sections[1].strip().split():
-        ratings_list = convert_to_list(line)
-        print('ratings_list', ratings_list)
+        ratings_dict = convert_ratings_to_dict(line)
+        ratings_list.append(ratings_dict)
+    print('ratings_list')
+    for e in ratings_list:
+        print(e)
 
-
+    print()
+    for ratings_dict in ratings_list:
+        print(ratings_dict, type(ratings_dict))
+        apply_workflow(ratings_dict, wf_dict)
